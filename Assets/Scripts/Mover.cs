e405c0ba0eddebde31444278e8adf397e459037a
Ji Feng <467318976@qq.com>
2017-06-18 19:11:08 +0800

    init

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody rigidbody = GetComponent<Rigidbody> ();
+		rigidbody.velocity = transform.forward * speed;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary {
+
+	public float xmin, xmax, zmin, zmax;
+
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		Rigidbody rigidbody = GetComponent<Rigidbody> ();
+		rigidbody.velocity = movement * speed;
+
+		rigidbody.position = new Vector3 (
+			Mathf.Clamp (rigidbody.position.x, boundary.xmin, boundary.xmax),
+			0.0f,
+			Mathf.Clamp (rigidbody.position.z, boundary.zmin, boundary.zmax)
+		);
+
+		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+	}
+}
